07a718ee815065dd9bb5cac4e33e5094add604ff
Ch-RR <2470521716@qq.com>
2025-01-25 03:30:29 +0000

    Save Post Draft source/_drafts/test.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/24/hello-world/","2025/01/24/test/"];function toRandomPost(){
+var posts=["2025/01/25/hello-world/","2025/01/24/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

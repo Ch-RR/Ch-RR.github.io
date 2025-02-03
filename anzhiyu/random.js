604a166b4b644f64f5ac23072fa868f52ed20318
Ch-RR <2470521716@qq.com>
2025-02-03 03:24:08 +0000

    Upload image.png by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/01/hello-world/","2025/01/24/test/"];function toRandomPost(){
+var posts=["2025/02/03/hello-world/","2025/01/24/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

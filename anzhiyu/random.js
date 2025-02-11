a5f3d09142800a1ae15a48372eed9f0984cb26d5
Ch-RR <2470521716@qq.com>
2025-02-11 02:04:31 +0000

    Publish Post source/_posts/旋量代数与李群、李代数.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/10/hello-world/","2025/01/24/test/","2025/02/07/旋量代数与李群、李代数/"];function toRandomPost(){
+var posts=["2025/02/11/hello-world/","2025/01/24/test/","2025/02/07/旋量代数与李群、李代数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

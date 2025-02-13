de1fc772bb51002be17b5a61101470893b382852
Ch-RR <2470521716@qq.com>
2025-02-13 06:44:02 +0000

    更新图片测试 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/12/hello-world/","2025/01/24/test/","2025/02/07/旋量代数与李群、李代数/"];function toRandomPost(){
+var posts=["2025/01/24/test/","2025/02/13/hello-world/","2025/02/07/旋量代数与李群、李代数/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

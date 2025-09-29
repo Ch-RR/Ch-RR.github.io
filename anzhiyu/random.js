ec3056abde866f8469a6ddec113881d3622b2902
Ch-RR <2470521716@qq.com>
2025-09-29 03:11:31 +0000

    Publish Post source/_posts/压缩感知(少就是多).md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/24/test/","2025/02/07/旋量代数与李群、李代数/","2025/08/31/维纳滤波器/"];function toRandomPost(){
+var posts=["2025/01/24/test/","2025/09/29/压缩感知(少就是多)/","2025/02/07/旋量代数与李群、李代数/","2025/08/31/维纳滤波器/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
